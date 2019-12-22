90781efc81d4b871176c87ba24c5b18756e782f4
Knlsharma <knlsharma678@gmail.com>
2019-12-22 22:27:37 +0530

    fixed

--- src/main/java/Library/Management/controller/Library_Controller.java
+++ src/main/java/Library/Management/controller/Library_Controller.java
@@ -62,5 +62,3 @@ public class Library_Controller {
 
 
 
-}
-
--- src/main/java/Library/Management/repository/dao/MappingRepository.java
+++ src/main/java/Library/Management/repository/dao/MappingRepository.java
@@ -2,7 +2,9 @@ package Library.Management.repository.dao;
 
 import Library.Management.entity.mapping;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MappingRepository extends CrudRepository<mapping,Integer>
 {
 
